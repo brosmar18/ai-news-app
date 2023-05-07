c34330cca354dc6ab5d1a30d7a528818a848c4a7
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-07 15:33:01 -0500

    alan ai account created and linked

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = process.env.REACT_APP_ALAN_KEY;
+console.log(alanKey);
+
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if(command === 'testCommand') {
+                    alert ('this code was executed');
+                }
+            },
+        });
+    }, []);
     return (
         <div>
             <h1>AI News Application</h1>
